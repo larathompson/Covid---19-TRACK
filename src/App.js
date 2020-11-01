27124d011982906797f252e55e4cadc35ba2af35
Lara Thompson <larathompson288@gmail.com>
2020-11-01 20:07:33 +0000

    first commit

---
+++ src/App.js
@@ -0,0 +1,126 @@
+import React, { useState, useEffect } from "react"
+import './App.css';
+import {
+  MenuItem,
+  FormControl,
+  Select,
+  Card,
+  CardContent
+} from "@material-ui/core"
+
+import InfoBox from './InfoBox'
+import Map from './Map'
+import Table from './Table'
+import './Table.css'
+import { sortData } from './util'
+import LineGraph from './LineGraph'
+// import 'leaflet/dist/leaflet.css'
+
+function App() {
+  const [countries, setCountries] = useState([])
+  //here we rememeber which country we selected - worldwide by default
+  const [country, setCountry] = useState('worldwide')
+  const [countryInfo, setCountryInfo] = useState({})
+  const [tableData, setTableData] = useState([])
+
+  useEffect(() => {
+    fetch('https://disease.sh/v3/covid-19/all')
+    .then(response => response.json())
+    .then(data => {
+      setCountryInfo(data)
+    })
+  }, [])
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      //you can also use axios but just using fetch here which is built into javadscript
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        //from the reponse we just want the json
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            //it is returning an object for all the countries
+            {
+              name: country.country,
+              value: country.countryInfo.iso2
+            }
+          ))
+          const sortedData = sortData(data)
+          setTableData(sortedData)
+          setCountries(countries)
+
+        })
+    }
+    getCountriesData()
+
+  }, [])
+
+  const onCountryChange = (event) => {
+    //this gets selected value
+    const countryCode = event.target.value
+    console.log("sdfsd", countryCode)
+    setCountry(countryCode)
+
+    const url = countryCode === 'worldwide'
+      ? 'https://disease.sh/v3/covid-19/all'
+      : `https://disease.sh/v3/covid-19/countries/${countryCode}`
+
+     fetch(url)
+      .then(response => response.json())
+      .then(data => {
+        setCountry(countryCode)
+        setCountryInfo(data)
+      })
+    //https://disease.sh/v3/covid-19/all
+    //https://disease.sh/v3/covid-19/countries/[COUNTRY_CODE]
+  }
+  console.log(countryInfo)
+
+
+  return (
+    <div className="app">
+      <div className="app__left">
+        <div className="app__header">
+          <h1>COVID-19 TRACKER</h1>
+          {/* this is material UI - this is BEM naming. This is for dropdown */}
+          <FormControl className="app__dropdown">
+            <Select variant="outlined"
+              onChange={onCountryChange}
+              value={country}>
+              {/* //here we are manually adding worldwide */}
+              <MenuItem value="worldwide">Worldwide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}> {country.name}</MenuItem>
+              ))}
+              {/* <MenuItem value="worldwide"> Worldwide</MenuItem>
+            <MenuItem value="worldwide"> Option 2</MenuItem>
+            <MenuItem value="worldwide"> dd</MenuItem>
+            <MenuItem value="worldwide"> dd</MenuItem> */}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox title="Coronavirus cases" cases={countryInfo.todayCases} total={countryInfo.cases}></InfoBox>
+          <InfoBox title="Recovered" cases={countryInfo.todayRecovered} total={countryInfo.recovered}></InfoBox>
+          <InfoBox title="Deaths" cases={countryInfo.todayDeaths} total={countryInfo.deaths}></InfoBox>
+        </div>
+
+        <Map></Map>
+      </div>
+
+      <Card className="app__right">
+        <CardContent>
+          <h3>Live Cases by Country</h3>
+          <Table countries={tableData}/>
+          <h3> Worldwide New Cases</h3>
+          <LineGraph />
+        </CardContent>
+      </Card>
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Map.js
@@ -0,0 +1,12 @@
+import React from "react"
+// import { Map as LeafletMap, TileLayer } from "react-leaflet"
+// import "./Map.css"
+// import { showDataOnMap } from "./util"
+
+function Map() {
+  return (
+    <h1>hi</h1>
+  )
+}
+export default Map
+
